61ea9f1dbbe696a25cefe8a1fafc18070221d49f
João Paulo Serodio <serodio@premiersoft.net>
2018-09-15 13:28:58 -0300

    Created the project.

---
+++ PegasusApp/PegasusApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PegasusApp
+//
+//  Created by João Paulo Serodio on 15/09/18.
+//  Copyright © 2018 PremierSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
+
